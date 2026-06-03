305717c3a8b9502cd0e3dc3b233b33ae51022b31
Alessandro <alexpiazza98@gmail.com>
2026-06-03 13:51:50 +0200

    eliminati file inutili

--- build.sh
+++
@@ -1,8 +0,0 @@
-
-php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-php composer-setup.php --install-dir=/tmp --filename=composer
-php -r "unlink('composer-setup.php');"
-
-/tmp/composer install --no-dev --optimize-autoloader
-
-npm ci
